9c609b2d26bfeb2023539cb24948a48ba327ca60
Elianamos <elianamos29@gmail.com>
2023-06-26 08:57:22 +0300

    fix code

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# Installs puppet-lint v2.5.0.
+# Installs flask v2.1.0.
 
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem'
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip'
 }
